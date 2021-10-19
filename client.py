9351bc8e01052eb051b60d1748eb6c40e591c86a
Гагарин Ярослав <gagarinyaroslav3@gmail.com>
2021-10-19 20:12:18 +0300

    ready

--- client.py
+++ client.py
@@ -2,6 +2,27 @@ import ipaddress
 import socket
 from ipaddress import ip_address
 
+
+def recv(connection):
+    head = int(connection.recv(1024).decode())
+    data = connection.recv(head)
+    return data.decode()
+
+
+def send(connection, message):
+    message = message.encode()
+
+    header = str(len(message)).encode()
+    connection.send(header)
+    connection.send(message)
+
+
+def respond(idd, message):
+    #send(sock, "!!!re!!!")
+    #send(sock, idd)
+    send(sock, str(message+"|"+str(idd)))
+
+
 sock = socket.socket()
 sock.setblocking(1)
 try:
@@ -15,13 +36,19 @@ except ValueError:
 sock.connect((ip, port))
 print("Соединение установлено")
 while True:
-    line = input()
-    sock.send(line.encode())
-    print("Данные отправлены")
-    if line == "exit":
-        print("Соединение разорвано")
-        break
-    data = sock.recv(1024)
+    line = input("Введите сообщение")
+    if line[:3] == "re:":
+        print("responded")
+        print(line[3], line[4:])
+        respond(line[3], line[4:])
+    else:
+        send(sock, line)
+        print("Данные отправлены")
+        if line == "exit":
+            print("Соединение разорвано")
+            break
+    print("responded")
+    data = recv(sock)
     print("Данные получены:")
-    print(data.decode())
+    print(data)
 sock.close()
--- server.py
+++ server.py
@@ -1,6 +1,19 @@
 import socket
 
 
+def recv(connection):
+	head = int(connection.recv(1024).decode())
+	data = connection.recv(head)
+	return data
+
+
+def send(connection, message):
+	message = message.encode()
+	header = str(len(message)).encode()
+	connection.send(header)
+	connection.send(message)
+
+
 def connect(port):
 	try:
 		sock.bind(("", port))
@@ -8,8 +21,27 @@ def connect(port):
 	except OSError:
 		connect(port+1)
 
+
+def respond(id, message):
+		send(sock, r"!!!re!!!")
+		send(sock, str(id).encode())
+		quote = recv(sock)
+		send(sock, str("responded to \"" + quote + "\"" + ":" + message).encode())
+
+
+def respond_recieved(data):
+	message, idd = data.split("|")
+	respond_to = messages[int(idd)]
+	to_send =  str("responed to \""+respond_to+"\":\n"+message)
+	print(to_send)
+	send(conn, to_send)
+	with open("log.txt", "a") as file:
+		file.write("Клиенту отправлены данные\n")
+
+
 with open("log.txt", "a") as file:
 	file.write("Сервер запущен\n")
+messages = []
 sock = socket.socket()
 try:
 	port = int(input("К какому порту подключаться"))
@@ -23,9 +55,8 @@ conn, addr = sock.accept()
 with open("log.txt", "a") as file:
 	file.write("Клиент подключен\n")
 msg = ''
-
 while True:
-	data = conn.recv(1024)
+	data = recv(conn)
 	with open("log.txt", "a") as file:
 		file.write("Приняты данные от клиента\n")
 	decoded = data.decode()
@@ -37,9 +68,12 @@ while True:
 		if continue_listening == "no":
 			conn.close()
 			break
+	elif "|" in decoded:
+		respond_recieved(decoded)
 	else:
+		messages.append(decoded)
 		msg += data.decode()
-		conn.send(data)
+		send(conn, data.decode())
 		with open("log.txt", "a") as file:
 			file.write("Клиенту отправлены данные\n")
 with open("log.txt", "a") as file:
